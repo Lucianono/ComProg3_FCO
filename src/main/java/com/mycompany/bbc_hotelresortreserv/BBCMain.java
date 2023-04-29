c437b9d2253a815bf800f76cd8ad8ca4c58a0db5
Bryan Calulo <bryancalulo@gmail.com>
2023-04-29 21:02:50 +0800

    remove unnecessaries

--- src/main/java/com/mycompany/bbc_hotelresortreserv/BBCMain.java
+++ src/main/java/com/mycompany/bbc_hotelresortreserv/BBCMain.java
@@ -4,12 +4,7 @@ public class BBCMain {
 
     public static void main(String[] args) {
         System.out.println("Hello Gudmorning!");
-<<<<<<< HEAD
-        
-        System.out.println("THIS IS MAIN!");
-=======
         System.out.println("Try");
         System.out.println("test 1!");
->>>>>>> try
     }
 }
