bc67380d32bd30d933ef88b7686b236967c1040b
MysteryMist <nsandiego14@gmail.com>
2023-05-01 14:44:33 +0800

    Planned variables so far

--- src/main/java/com/mycompany/bbc_hotelresortreserv/HotelCRUD.java
+++ src/main/java/com/mycompany/bbc_hotelresortreserv/HotelCRUD.java
@@ -17,10 +17,47 @@ TODO :
 
 
 public class HotelCRUD {
-    
-    HotelCRUD(){
-        System.out.println("HotelCRUD created");
+/*
+public String name;
+public int numofroom;
+public int[] roomcap;
+public int availableroom;
+
+    HotelCRUD(String name, int numofroom, int roomcap){
+        this.name = name;
+        this.numofroom = numofroom;
+
     }
 
+    public void availability(String name, int numofroom, int roomcap){
+        System.out.print ("[1] Tourist Class");
+        System.out.print ("[2] Deluxe Class");
+        System.out.print ("[3] Ambassador Class");
+        System.out.print ("[4] Corporate Class");
+        System.out.print ("[5] Annex Room 1");
+        System.out.print ("[6] Annex Room 2");
+        if (numofroom < 1 || numofroom >6){
+            System.out.print("Invalid room number, Please choose between 1 to 6.");
+        }
+        if (availableroom <= 0){
+            System.out.print("No room available");
+        }
+
+    }
     
+    public void reserve(String name, int numofroom, int roomcap){
+    }
+
+    public void get(String name, int numofroom, int roomcap){
+        
+    }
+    
+    public void update(String name, int numofroom, int roomcap){
+        
+    }
+    
+    public void delete(String name, int numofroom, int roomcap){
+        
+    }
+*/
 }
