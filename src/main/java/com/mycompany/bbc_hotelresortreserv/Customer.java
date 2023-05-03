d2630a4053f77870facd3cf26d765c75a7f0fb92
Jelo Rosal <jelorosal9@gmail.com>
2023-05-03 19:42:59 +0800

    Customercrud finished, linked list to be added

--- src/main/java/com/mycompany/bbc_hotelresortreserv/Customer.java
+++ src/main/java/com/mycompany/bbc_hotelresortreserv/Customer.java
@@ -1,30 +1,48 @@
 package com.mycompany.bbc_hotelresortreserv;
-
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
+import java.text.SimpleDateFormat;
+import java.time.LocalDateTime; 
 
-/**
- *
- * @author Darwin
- */
 public class Customer {
     String cstmrName;
     int cstmrAge;
-    long timeChecked = 0;
-    int custNo;
-    private final String name;
-    private final int age;
+    int ID;
+    int cstmrTime;
 
-    public Customer(int custNo, String name, int age, long timeChecked) {
-        this.name = name;
-        this.age = age;
-        this.timeChecked = timeChecked;
-        this.custNo = custNo;
+
+    public Customer(String name, int age, int ID) {
+        this.cstmrName = cstmrName;
+        this.cstmrAge = cstmrAge;
+        this.ID = ID;
+    }
+
+    Customer() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
     }
 
     Object getName() {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        return cstmrName;
     }
+
+    int getAge() {
+        return cstmrAge;
+    }
+
+    void setAge(int age) {
+        this.cstmrAge = cstmrAge;
+    }
+
+    void setName(String newName) {
+        this.cstmrName = cstmrName;
+    }
+
+    Date getTimeChecked() {
+        SimpleDateFormat formatter = new SimpleDateFormat("dd/MM/yyyy HH:mm");  
+        Date cstmrTime = new Date();
+        return cstmrTime;
+    }
+
 }
--- src/main/java/com/mycompany/bbc_hotelresortreserv/CustomerCRUD.java
+++ src/main/java/com/mycompany/bbc_hotelresortreserv/CustomerCRUD.java
@@ -12,49 +12,88 @@ TODO :
 -Customer should have the following details: Name, Age, TimeChecked
 
 */
-import java.util.*;
+/*This is the class for Customer CRUD
+
+TODO :
+-This do not have GUI
+-Create methods for Customer Information System
+-Create method to create a customer
+-Create method to read or get a customer's details
+-Create method to update a customer's details
+-Create method to delete a customer
+-Customer should have the following details: Name, Age, TimeChecked
+
+*/
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
+
+
 
 public class CustomerCRUD {
     
+        public static void main(String[] args) { // FOR TESTING ONLY
+        Scanner sc = new Scanner(System.in);
+        Customer customer = new Customer();
+ 
+
+// To be Completed
+        }
+        
     private static int cstmrNum;
     private List<Customer> customers;
 
     CustomerCRUD() {
         customers = new ArrayList<>();
-        cstmrNum = 0;
     }
 
-    public void createCustomer() {
-        
+
+    public CustomerCRUD(List<Customer> customers) { //constructor with para for creating customer objects
+        this.customers = customers;
+        cstmrNum = customers.size();
+    }
+
+    public void createCustomer(Customer customer) { //create customer
         cstmrNum++;
+        customers.add(customer);
     }
 
-    
-    public Customer getCustomer(String name) {
-       
-        for (Customer c : customers) {
-            if (c.getName().equals(name)) {
-                return c;
+    public int getIndex(int ID) {//get index 
+        for (int i = 0; i < customers.size(); i++){
+            if (customers.get(i).equals(ID)) {
+                return i;
             }
         }
-        return null;
-    }
-
-    public void updateCustomer(String name) {
-        
-    }
-
-    public void deleteCustomer(String name) {
-        
-        cstmrNum--;
-    }
-
-    public int getNumCustomers() {
-        return customers.size();
+        return -1;
     }
 
 
-    
+    public void updateCustomerName(int ID, String cstmrName) { //changing of customer name
+        for (Customer c : customers) {
+            if (c.getName().equals(ID)) {
+            }
+        }
+    }
+    public void updateCustomerAge(int ID, int cstmrAge) { //chaging of customer age
+        for (Customer c : customers) {
+            if (c.getName().equals(ID)) {
+            }
+        }
+    }  
+
+    public void deleteCustomer(int ID, String cstmrName, int cstmrAge) { //remove customer depending on index
+        if (getIndex(ID) != -1)
+        {
+            customers.remove(ID);
+        }
+        else {
+        }
+    }
+
+    public void displayCustomers() {//display customers along with name, age, time checked
+        for (Customer c : customers) {
+            System.out.println(c.getName() + ", " + c.getAge() + ", " + c.getTimeChecked());
+        }
+    }
+
 }
