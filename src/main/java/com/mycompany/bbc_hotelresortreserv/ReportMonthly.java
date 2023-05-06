d383b907515cf0f1cce4c73a6079beb155f78fb3
MysteryMist <nsandiego14@gmail.com>
2023-05-06 20:16:16 +0800

    added another frame but doesn't work atm

--- src/main/java/com/mycompany/bbc_hotelresortreserv/ReportMonthly.java
+++ src/main/java/com/mycompany/bbc_hotelresortreserv/ReportMonthly.java
@@ -1,5 +1,6 @@
 package com.mycompany.bbc_hotelresortreserv;
 import javax.swing.*;
+import java.awt.event.*;
 
 /*
 This class is for Monthly Report JFrame
@@ -17,20 +18,55 @@ TODO:
 */
 
 public class ReportMonthly {
-    
+
+private JButton nxtbtn = new JButton("Next");
+
     public ReportMonthly() {
-        JFrame frm = new JFrame();
+        JFrame mfrm = new JFrame("Monthly Report");
+        
         String data[][] = {{"101", "Amit", "670000"},
                             {"102", "Jai", "780000"},
                             {"101", "Sachin", "700000"}};
+        
         String column[] = {"Sales", "#ofCustomers", "ResourcesUsed"};
+        
         JTable rm = new JTable(data, column);
-        rm.setBounds(30, 40, 200, 300);
+        
         JScrollPane sp = new JScrollPane(rm);
-        frm.add(sp);
-        frm.setSize(300, 400);
-        frm.setVisible(true);
-        frm.setResizable(false);
-        frm.setLocationRelativeTo(null);
+        
+        rm.setBounds(30, 40, 200, 300);
+        
+        mfrm.add(sp);
+        mfrm.setSize(1390, 737);
+        mfrm.setVisible(true);
+        mfrm.setResizable(false);
+        mfrm.setLocationRelativeTo(null);
+        
+        mfrm.setDefaultCloseOperation(JFrame.HIDE_ON_CLOSE);
+        mfrm.getContentPane().add(nxtbtn);
+        
+        nxtbtn.addActionListener(new ActionListener(){
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                mfrm.dispose();
+                SecondFrame sfrm = new SecondFrame();
+            }
+        });
+        
+        JPanel panel = new JPanel();
+        panel.add(nxtbtn);
+        mfrm.getContentPane().add(panel, "South");
+        
+        mfrm.setVisible(true);
+    }
+
+    private static class SecondFrame {
+        private JFrame sfrm = new JFrame("Monthly Report");
+        public SecondFrame() {
+            sfrm.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            sfrm.setSize(300,400);
+            sfrm.setVisible(true);
+            sfrm.setResizable(false);
+        }
     }
 }
