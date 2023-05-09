cf4ab9bc00290c1b3e1b91abbab3fa099daf3974
Unknown <meepogaming125@gmail.com>
2023-05-09 08:16:49 +0800

    GUI to login created

--- src/main/java/com/mycompany/bbc_hotelresortreserv/LoginSignupFormGUI.java
+++ src/main/java/com/mycompany/bbc_hotelresortreserv/LoginSignupFormGUI.java
@@ -1,6 +1,15 @@
 
 package com.mycompany.bbc_hotelresortreserv;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JPasswordField;
+import javax.swing.JTextField;
+
 /*
 This is a security extended, Login and Signup Form
 
@@ -15,10 +24,69 @@ TODOs:
 -BE CREATIVE! BE Mindful on the layouts. Layouts should be responsive
 
 */
-public class LoginSignupFormGUI {
+public class LoginSignupFormGUI implements ActionListener{
     
     LoginSignupFormGUI(){
         System.out.println("LoginSignupFormGUI created");
+        
+        JPanel panel = new JPanel();
+        //JButton button = new JButton();
+        JFrame frame = new JFrame();
+        
+        
+       // button.setBounds(150, 150, 50, 20);
+                
+        //JPanel Login = new JPanel();
+        
+        //JPanel Password = new JPanel();
+        panel.setLayout(null);
+        
+        JLabel username = new JLabel("Username : ");
+        username.setBounds(180, 20, 250, 250);
+        panel.add(username);
+        
+        JLabel password = new JLabel("Password : ");
+        password.setBounds(180, 50, 250, 250);
+        panel.add(password);
+        
+        JTextField userText = new JTextField(20);
+        userText.setBounds(250, 131, 165, 25);
+        panel.add(userText); 
+        
+        /*
+        JTextField passText = new JTextField(20);
+        passText.setBounds(250, 161, 165, 25);
+        panel.add(passText);
+        */
+        
+        //encrypts password
+        JPasswordField passwordText = new JPasswordField();
+        passwordText.setBounds(250,161,165,25);
+        panel.add (passwordText);
+        
+        JButton loginButton = new JButton("Login");
+        loginButton.setBounds(250, 191, 80, 25);
+        loginButton.addActionListener(new LoginSignupFormGUI());
+        panel.add(loginButton);
+        
+        
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setResizable(false);
+        frame.setTitle("Login and SignUp Form");
+        frame.setSize(600,400);
+        frame.setVisible(true);
+        frame.setLayout(null);
+        frame.add(username);
+        frame.add(password);
+       // frame.add(button);
+        frame.add(panel);
+        frame.add(userText);
+       // frame.add(passText);
+        frame.add(passwordText);
+        frame.add(loginButton);
+    }
+    public void actionPerformed(ActionEvent e){
+        System.out.println("asd");
     }
     
 }
