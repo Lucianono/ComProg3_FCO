84f1e794a3e1deaf3483fe3b6bd156fb62cbb217
Bryan Calulo <bryancalulo@gmail.com>
2023-05-14 00:07:24 +0800

    hotel add; buggy

--- src/main/java/com/mycompany/bbc_hotelresortreserv/HotelRoomGUI.java
+++ src/main/java/com/mycompany/bbc_hotelresortreserv/HotelRoomGUI.java
@@ -96,6 +96,7 @@ public class HotelRoomGUI extends JFrame implements ActionListener{
 
         addHotelBtn.setFont(new Font("Verdana", 1, 14)); // NOI18N
         addHotelBtn.setText("ADD HOTEL ROOM");
+        addHotelBtn.addActionListener(this);
         jPanel6.add(addHotelBtn, BorderLayout.CENTER);
 
         jPanel2.add(jPanel6);
@@ -303,9 +304,24 @@ public class HotelRoomGUI extends JFrame implements ActionListener{
     
     @Override
     public void actionPerformed(ActionEvent e) {
+            
+        hotelSys.addHotel(JOptionPane.showInputDialog("Enter HotelID"),
+                        JOptionPane.showInputDialog("Enter Hotel Type"),true,
+                        Integer.parseInt(JOptionPane.showInputDialog("Enter Room Capacity")),
+                        Double.parseDouble(JOptionPane.showInputDialog("Enter Regular Rate")),
+                        Double.parseDouble(JOptionPane.showInputDialog("Enter Promo Rate")));
+                refreshHotelSuperPanel();
         
-    }
-    
-    
+//            try {
+//                hotelSys.addHotel(JOptionPane.showInputDialog("Enter HotelID"),
+//                        JOptionPane.showInputDialog("Enter Hotel Type"),true,
+//                        Integer.parseInt(JOptionPane.showInputDialog("Enter Room Capacity")),
+//                        Double.parseDouble(JOptionPane.showInputDialog("Enter Regular Rate")),
+//                        Double.parseDouble(JOptionPane.showInputDialog("Enter Promo Rate")));
+//                refreshHotelSuperPanel();
+//            } catch (Exception err) {
+//                JOptionPane.showMessageDialog(null, "Incorrect input!" );
+//            }
+    }  
     
 }
