777ba443d35b25902de1b935a15f0a444c1933b7
Noren Ester Belonio <norenesterbelonio0107@gmail.com>
2023-05-14 23:47:21 +0800

    Nagitna na Picture at Copyrights na lang

--- src/main/java/com/mycompany/bbc_hotelresortreserv/SplashScreen.java
+++ src/main/java/com/mycompany/bbc_hotelresortreserv/SplashScreen.java
@@ -12,27 +12,40 @@ TODO:
 */
 package com.mycompany.bbc_hotelresortreserv;
 
+import java.awt.Color;
 import javax.swing.*;
 import java.awt.FlowLayout;
+import java.awt.Font;
+import java.awt.GridBagLayout;
 import java.awt.event.*;
 import javax.swing.JProgressBar;
 
 
 public class SplashScreen {
     JFrame frm = new JFrame();
+    JLabel title = new JLabel("Black Beans Company");
     JProgressBar bar = new JProgressBar(0,100);
+    
     
     SplashScreen(){
     bar.setValue(0);
     bar.setBounds(0, 0, 400, 40);
     bar.setStringPainted(true);
-     
+    bar.setFont(new Font("Arial",Font.BOLD,9));
+    bar.setForeground(Color.BLUE);
+    bar.setBackground(Color.WHITE);
+    
+    title.setVerticalAlignment(SwingConstants.CENTER);
+    title.setHorizontalAlignment(SwingConstants.CENTER);
+    
+    frm.add(title);
     frm.add(bar);
-    frm.setLayout(new FlowLayout(FlowLayout.CENTER));
+    frm.setLayout(new GridBagLayout());
     frm.setSize(600, 400);
     frm.setVisible(true);
     frm.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     frm.setLocationRelativeTo(null);
+    
     fill();
     
     }
