61653ab81857aa8f3bd3e5f8c3c408e569ef60a3
Noren Ester Belonio <norenesterbelonio0107@gmail.com>
2023-05-15 21:14:32 +0800

    Help About with sample text

--- src/main/java/com/mycompany/bbc_hotelresortreserv/HelpAbout.java
+++ src/main/java/com/mycompany/bbc_hotelresortreserv/HelpAbout.java
@@ -7,12 +7,16 @@ import java.awt.*;
 public class HelpAbout {
     JFrame frm = new JFrame();
     JLabel about = new JLabel("About");
-
+    JLabel abt = new JLabel("This system wwas all about how to ama"); 
+    
     public HelpAbout() {
     about.setFont(new Font("Arial",Font.BOLD,25)); 
     
-    frm.add(about);     
-    frm.setLayout(new FlowLayout(FlowLayout.CENTER,10,15)); 
+    abt.setFont(new Font("Arial",Font.PLAIN,12)); 
+    
+    frm.add(about);
+    frm.add(abt);     
+    frm.setLayout(new FlowLayout(FlowLayout.CENTER,600,5)); 
     frm.setSize(600, 400); 
     frm.setVisible(true); 
     frm.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); 
