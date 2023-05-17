8d0800b30007a22b436935e414d5593d9ad023f2
Bryyyyy <bryancalulo@gmail.com>
2023-05-17 11:27:22 +0800

    even more simplified

--- src/main/java/com/mycompany/bbc_hotelresortreserv/Report.java
+++ src/main/java/com/mycompany/bbc_hotelresortreserv/Report.java
@@ -24,36 +24,145 @@ TODO:
 
 public class Report{
     
+    private JButton rsrcsbtn = new JButton("Resources Used");
+    private JButton backbtn = new JButton("Back to Report");
+    private JButton nxtbtn = new JButton("Next Year");
+    private final String[] months = {"January", "February", "March", "April", 
+        "May", "June", "July", "August", "September", "October", "November", "December"};
+    private final String[] columnNames = {"Day" , "Sales", "No of Customers"};
+    private int yearSelected = 2023;
+    
     public Report() {
-        JFrame main = new JFrame("Report");
-        JPanel panel = new JPanel(new BorderLayout());
-
-        JLabel label = new JLabel("Select a year:");
-        label.setHorizontalAlignment(JLabel.CENTER);
-        panel.add(label, BorderLayout.NORTH);
-
-        ButtonGroup yrs = new ButtonGroup();
-        JButton yr1 = new JButton("2023");
-
-        yrs.add(yr1);
-
-        JPanel buttonPanel = new JPanel(new GridLayout(3,1));
-        buttonPanel.add(yr1);
-        panel.add(buttonPanel, BorderLayout.CENTER);
-
-        yr1.addActionListener(new ActionListener() {
+        JFrame mfrm = new JFrame(yearSelected+"");
+        JPanel mpnl = new JPanel();
+        
+        for (int monthIndex = 0; monthIndex < months.length; monthIndex++) {
+            String[][] rowData = new String[getMaxDays(monthIndex)][3];
+            for (int dayIndex = 0; dayIndex < rowData.length; dayIndex++) {
+                rowData[dayIndex][0] = Integer.toString(dayIndex + 1);
+            }
+            JTable table = new JTable(rowData, columnNames){
+                 @Override
+                    public boolean isCellEditable(int row, int column) {
+                       //all cells false
+                       return false;
+                    }
+            };
+            JPanel tblPnl = new JPanel ();
+            
+            table.getColumnModel().getColumn(0).setMaxWidth(50);
+            JScrollPane scrollPane = new JScrollPane(table);
+            scrollPane.setPreferredSize(new Dimension(500,200));
+            
+            tblPnl.add (scrollPane);
+            
+            JLabel label = new JLabel(months[monthIndex]);
+            mpnl.add(label);
+            mpnl.add(tblPnl);
+        }
+        
+        mpnl.setLayout(new BoxLayout(mpnl, BoxLayout.PAGE_AXIS));
+        
+        JScrollPane scrollPane = new JScrollPane(mpnl);
+        scrollPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
+        
+        mfrm.pack();
+        mfrm.add(scrollPane);
+        mfrm.setSize(700, 500);
+        mfrm.setVisible(true);
+        mfrm.setResizable(false);
+        mfrm.setLocationRelativeTo(null);
+        
+        rsrcsbtn.addActionListener(new ActionListener(){
             @Override
             public void actionPerformed(ActionEvent e) {
-                yr1 yr2023 = new yr1();
-                main.dispose();
+                SecondFrame sfrm = new SecondFrame();
             }
         });
+   
+        backbtn.addActionListener(new ActionListener(){
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                mfrm.dispose();
+            }
+        });
+        
+        nxtbtn.addActionListener(new ActionListener(){
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                //next year
+            }
+        });
+        
+        JPanel buttonPanel = new JPanel(new GridLayout(1, 2));
+        buttonPanel.add(backbtn);
+        buttonPanel.add(rsrcsbtn);
+        buttonPanel.add(nxtbtn);
+        
+        mfrm.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);     
+        mfrm.getContentPane().add(buttonPanel, "South");
+    }
+  
     
-        main.setContentPane(panel);
-        main.setSize(600, 400);
-        main.setVisible(true);
-        main.setResizable(false);
-        main.setLocationRelativeTo(null);
-        main.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);     
+    
+    
+    public class SecondFrame extends JFrame {
+   
+    private final String[] columnNames2 = {"Day", "Extra Bed", "Pillow", 
+        "Towel", "Reading Light", "Books/Magazine", "Toiletries", 
+        "Luggage Rack", "Breakfast", "Lunch", "Dinner", "Drink", "Snack"};
+    
+    private JButton backbtn = new JButton("Back to Year Report");
+    
+    public SecondFrame() {
+        JFrame sfrm = new JFrame("2023");
+        JPanel mpnl2 = new JPanel();
+        mpnl2.setLayout(new BoxLayout(mpnl2, BoxLayout.PAGE_AXIS));
+
+        for (int monthIndex = 0; monthIndex < months.length; monthIndex++) {
+            String[][] rowData = new String[getMaxDays(monthIndex)][columnNames2.length];
+            for (int dayIndex = 0; dayIndex < rowData.length; dayIndex++) {
+                rowData[dayIndex][0] = Integer.toString(dayIndex + 1);
+            }
+            JTable table = new JTable(rowData, columnNames2);
+            table.setAutoResizeMode(JTable.AUTO_RESIZE_OFF);
+            table.getColumnModel().getColumn(0).setPreferredWidth(50);
+            table.setPreferredScrollableViewportSize(new Dimension(950, 400));
+            JScrollPane scrollPane = new JScrollPane(table);
+            JLabel label = new JLabel(months[monthIndex]);
+            mpnl2.add(label);
+            mpnl2.add(scrollPane);
+        }
+
+        JScrollPane scrollPane = new JScrollPane(mpnl2);
+        scrollPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
+        
+        sfrm.getContentPane().add(scrollPane);
+
+        sfrm.pack();
+        sfrm.setSize(600, 400);
+        sfrm.setVisible(true);
+        sfrm.setResizable(true);
+        sfrm.setLocationRelativeTo(null);
+        sfrm.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        
+        backbtn.addActionListener(new ActionListener(){
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                dispose();
+                yr1 yr2023 = new yr1();
+            }
+        });
+        
+        sfrm.getContentPane().add(backbtn, "South");
+    }
+}
+    
+    private int getMaxDays(int monthIndex) { 
+        return switch (monthIndex) {
+            case 1 -> 28;
+            case 3, 5, 8, 10 -> 30;
+            default -> 31;
+        };
     }
 }
