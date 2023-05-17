b7a75e3b07f8682b3a6f9d0bcd12522a135ad321
Noren Ester Belonio <norenesterbelonio0107@gmail.com>
2023-05-17 10:23:01 +0800

    About done
    About done

--- src/main/java/com/mycompany/bbc_hotelresortreserv/HelpAbout.java
+++ src/main/java/com/mycompany/bbc_hotelresortreserv/HelpAbout.java
@@ -8,20 +8,53 @@ import javax.swing.border.EmptyBorder;
 public class HelpAbout {
     JFrame frm = new JFrame();
     JLabel about = new JLabel("About the Developer");
-    JLabel abt = new JLabel("From BSM CS 2A");
-    //The Developer of Hotel and Resort Reservation System for Black Beans Company
-    JPanel pAbt;
+    JLabel abt = new JLabel("Presented By:");
+    JLabel name1 = new JLabel("Darwin Alba Inigo I. Alba             Noren Ester C. Belono             Bryan Jett T. Calulo");
+    JLabel ys1 = new JLabel(      "   BSM CS 2A - G1                          BSM CS 2A - G1                       BSM CS 2A - G1");
+    JLabel phonNum1 = new JLabel( "  0912-700-2544                            0912-700-2544                          0912-700-2544");
+    JLabel blnk1 = new JLabel(" ");
+    JLabel name2 = new JLabel("Ramil Angelo Jan M. Rosal             Neil Allen H. Sandiego");
+    JLabel ys2 = new JLabel(      "   BSM CS 2A - G1                          BSM CS 2A - G1");
+    JLabel phonNum2 = new JLabel( "  0912-700-2544                            0912-700-2544");
+    JLabel blnk2 = new JLabel(" ");
+    JLabel pt = new JLabel("Presented To:");
+    JLabel name3 = new JLabel("Engr. Evelyn C. Samson");
+    JLabel intruc = new JLabel("Instruvtor, MCS 203 ");
+    
+    JLabel blnk3 = new JLabel(" ");
+    JLabel help = new JLabel("Help");
     
     public HelpAbout() {
-        this.pAbt = new JPanel(new FlowLayout(FlowLayout.LEFT));
-    about.setFont(new Font("Arial",Font.BOLD,25)); 
-    
-    abt.setFont(new Font("Arial",Font.PLAIN,12)); 
+    about.setFont(new Font("Arial",Font.BOLD,20)); 
+    abt.setFont(new Font("Arial",Font.BOLD,12)); 
     abt.setBorder(new EmptyBorder(0,0,25,10));
-    pAbt.add(abt);      
+    
+    name1.setFont(new Font("Arial",Font.BOLD,12)); 
+    ys1.setFont(new Font("Arial",Font.PLAIN,12)); 
+    phonNum1.setFont(new Font("Arial",Font.PLAIN,12)); 
+    name2.setFont(new Font("Arial",Font.BOLD,12)); 
+    ys2.setFont(new Font("Arial",Font.PLAIN,12)); 
+    phonNum2.setFont(new Font("Arial",Font.PLAIN,12)); 
+    name3.setFont(new Font("Arial",Font.BOLD,12)); 
+    intruc.setFont(new Font("Arial",Font.PLAIN,12)); 
+    
+    help.setFont(new Font("Arial",Font.BOLD,20)); 
     
     frm.add(about);
-    frm.add(pAbt);
+    frm.add(abt); 
+    frm.add(name1);
+    frm.add(ys1);
+    frm.add(phonNum1);
+    frm.add(blnk1);
+    frm.add(name2);
+    frm.add(ys2);
+    frm.add(phonNum2);
+    frm.add(blnk2);
+    frm.add(pt);
+    frm.add(name3);
+    frm.add(intruc);    
+    frm.add(blnk3);
+    frm.add(help);
     frm.setLayout(new FlowLayout(FlowLayout.CENTER,600,5)); 
     frm.setSize(600, 400); 
     frm.setVisible(true); 
